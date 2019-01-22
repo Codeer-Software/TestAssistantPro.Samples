6c98cc39b12d36f8e6e0f07ae33f78246aabe508
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2019-01-23 01:02:32 +0900

    implement samples.

---
+++ WPF/Driver.InTarget/WindowAnalysisMenuAction.cs
@@ -0,0 +1,33 @@
+﻿using Codeer.TestAssistant.GeneratorToolKit;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Driver.InTarget
+{
+    public class WindowAnalysisMenuAction : IWindowAnalysisMenuAction
+    {
+        public Dictionary<string, MenuAction> GetAction(object target, WindowAnalysisTreeInfo info)
+        {
+            var dic = new Dictionary<string, MenuAction>();
+
+            var grid = target as DataGrid;
+            if (grid != null)
+            {
+                dic["Create Grid Column Define."] = () =>
+                {
+                    AnalyzeWindow.Output.Show();
+
+                    AnalyzeWindow.Output.WriteLine("public class GridColumns");
+                    AnalyzeWindow.Output.WriteLine("{");
+                    int i = 0;
+                    foreach (var e in grid.Columns)
+                    {
+                        AnalyzeWindow.Output.WriteLine("    const int " + e.Header.ToString() + " = " + i++ + ";");
+                    }
+                    AnalyzeWindow.Output.WriteLine("}");
+                };
+            }
+            return dic;
+        }
+    }
+}
---
+++ WPF/Driver/Tools/CapterAttachTreeMenuAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Codeer.TestAssistant.GeneratorToolKit;
+
+namespace Driver.Tools
+{
+    public class CapterAttachTreeMenuAction : ICapterAttachTreeMenuAction
+    {
+        public Dictionary<string, MenuAction> GetAction(string accessPath, object driver)
+        {
+            var dic = new Dictionary<string, MenuAction>();
+
+            dic["Assert"] = () =>
+            {
+                foreach (var e in driver.GetType().GetProperties())
+                {
+                    var obj = e.GetValue(driver);
+                    if (obj == null) continue;
+                    var textProp = obj.GetType().GetProperty("Text");
+                    if (textProp == null) continue;
+
+                    var text = textProp.GetValue(obj).ToString();
+
+                    CaptureAdaptor.AddCode("Assert.AreEqual(" + accessPath + "." + e.Name + ".Text, \"" + text + "\");");
+                }
+            };
+
+            return dic;
+        }
+    }
+}
